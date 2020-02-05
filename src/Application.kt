d92afb867bc0027d2224f6220a961fc372ff0a35
Yaroslav Ovdiienko <uptechandroidteam@Yaroslavs-MacBook-Pro.local>
2020-02-05 18:02:06 +0200

    - add multi-threading support

--- src/Application.kt
+++ src/Application.kt
@@ -6,7 +6,6 @@ import com.google.gson.JsonParser
 import com.google.gson.annotations.SerializedName
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.application.install
 import io.ktor.application.log
 import io.ktor.client.HttpClient
 import io.ktor.client.call.call
@@ -15,10 +14,6 @@ import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.content.TextContent
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
-import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
@@ -28,7 +23,8 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
-import org.slf4j.event.Level
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import team.uptech.food.bot.bot.Bot
 import team.uptech.food.bot.bot.BotReplay
 import team.uptech.food.bot.data.DataStorage
@@ -36,25 +32,17 @@ import team.uptech.food.bot.data.Storage
 import team.uptech.food.bot.presentation.messages.MessageBuilder
 import team.uptech.food.bot.presentation.modals.ModalBuilder
 import team.uptech.food.bot.slack.API
+import team.uptech.food.bot.utils.configurate
 import team.uptech.food.bot.utils.getBotToken
 import team.uptech.food.bot.utils.getToken
 
+
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-// Referenced in application.conf
 @Suppress("unused")
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-
-  install(DefaultHeaders)
-
-  install(ContentNegotiation) {
-    gson { setPrettyPrinting() }
-  }
-
-  install(CallLogging) {
-    level = Level.DEBUG
-  }
+  configurate()
 
   val client = HttpClient(Apache)
   val botReplay = BotReplay()
@@ -70,94 +58,82 @@ fun Application.module(testing: Boolean = false) {
       // TODO: check if a user has already started order ( decline drops after successful or canceled order )
       // TODO: make sure that we locked on channel from where user requested the order
       val parameters = call.receiveParameters()
-      log.debug(parameters.entries().toString())
+      withContext(Dispatchers.Default) { log.debug(parameters.entries().toString()) }
       val triggerId = parameters[API.TRIGGER_ID]
       val userId = parameters[API.USER_ID]
       val channelId = parameters[API.CHANNEL_ID]
 
-      if (!triggerId.isNullOrBlank()) {
-        // TODO: extract into API extension
-        // TODO: pass type of modal and assemble it
-        val response = client.call(API.VIEW_OPEN) {
-          method = HttpMethod.Post
-          header(API.HEADER, API.header(getToken()))
-          body = TextContent(ModalBuilder.assembleNewOrder(triggerId), contentType = ContentType.Application.Json)
-        }.response
+      withContext(Dispatchers.Default) { /* TODO: save 👆🏻 into Storage */ }
 
-        // get "view":{"id":"VS5L99ZD1" to update modal, keep it
-        log.debug(response.call.receive())
-      }
+      withContext(Dispatchers.Default) {
+        if (!triggerId.isNullOrBlank()) {
+          // TODO: extract into API extension
+          // TODO: pass type of modal and assemble it
+          val response = client.call(API.VIEW_OPEN) {
+            method = HttpMethod.Post
+            header(API.HEADER, API.header(getToken()))
+            body = TextContent(ModalBuilder.assembleNewOrder(triggerId), contentType = ContentType.Application.Json)
+          }.response
 
-      // TODO: this profile info of initiator should be saved in future
-      val response = client.call(API.USER_PROFILE) {
-        method = HttpMethod.Get
-        header(API.HEADER, API.header(getToken()))
-        parameter("token", getBotToken())
-        parameter("user", userId)
-      }.response
-
-      response.call.receive<String>().also {
-        if (it.isNotBlank()) {
-          userProfile = JsonParser().parse(it) as JsonObject
-          log.debug(it)
+          log.debug(response.call.receive())
         }
       }
 
-//            call.respondText(getMessage(), ContentType.Application.Json)
-      call.respond(HttpStatusCode.OK, "")
-
-      /* keep for future usage ->>
-      val response = client.call(API.SEND_MSG) {
-          method = HttpMethod.Post
+      withContext(Dispatchers.Default) {
+        // TODO: this profile info of initiator should be saved in future
+        val response = client.call(API.USER_PROFILE) {
+          method = HttpMethod.Get
           header(API.HEADER, API.header(getToken()))
-          body = TextContent(getMessage(), contentType = ContentType.Application.Json)
-      }.response
+          parameter("token", getBotToken())
+          parameter("user", userId)
+        }.response
 
-      log.debug(response.call.receive())
-      */
+        response.call.receive<String>().also {
+          if (it.isNotBlank()) {
+            userProfile = JsonParser().parse(it) as JsonObject
+            log.debug(it)
+          }
+        }
+      }
+
+      call.respond(HttpStatusCode.OK, "")
     }
 
     post(Bot.MANUAL_RESET_PATH) {
-      val parameters = call.receiveParameters()
-      log.debug(parameters.entries().toString())
-//            val userId = parameters[API.USER_ID]
-      // TODO: drop all data related to user and his initiated an order.
-      //  consider add confirmation dialog if user just wanted to check how it behaves in the middle of the order
-      call.respondText("${userProfile["profile"].asJsonObject["real_name"].asString}, you now able to initiate a new order :stew:")
+      withContext(Dispatchers.Default) {
+        val parameters = call.receiveParameters()
+        log.debug(parameters.entries().toString())
+        // TODO: drop all data related to user and his initiated an order.
+        //  consider add confirmation dialog if user just wanted to check how it behaves in the middle of the order
+        call.respondText("${userProfile["profile"].asJsonObject["real_name"].asString}, you now able to initiate a new order :stew:")
+      }
     }
 
     post(Bot.USER_INTERACTIONS) {
-      val parameters = call.receiveParameters()
-      log.debug(parameters.entries().toString())
+      withContext(Dispatchers.Default) {
+        val parameters = call.receiveParameters()
+        log.debug(parameters.entries().toString())
 
-      val view = Gson().fromJson(parameters["payload"], CallbackId::class.java)
+        val view = Gson().fromJson(parameters["payload"], CallbackId::class.java)
 
-      if (view?.view != null && "order_initiation" == view.view.callbackId) {
-        val response = client.call(API.SEND_MSG_EPH) {
-          method = HttpMethod.Post
-          header(API.HEADER, API.header(getToken()))
-          body = TextContent(
-            MessageBuilder.assembleOrderSettings(getBotToken(), view.user.id),
-            contentType = ContentType.Application.Json
-          )
-        }.response
-        log.debug(response.call.receive())
+        if (view?.view != null && "order_initiation" == view.view.callbackId) {
+          val response = client.call(API.SEND_MSG_EPH) {
+            method = HttpMethod.Post
+            header(API.HEADER, API.header(getToken()))
+            body = TextContent(
+              MessageBuilder.assembleOrderSettings(getBotToken(), view.user.id),
+              contentType = ContentType.Application.Json
+            )
+          }.response
+          log.debug(response.call.receive())
 
-        call.respond(HttpStatusCode.OK, "")
-      } else {
-        call.respond(HttpStatusCode.OK, "")
+          call.respond(HttpStatusCode.OK, "")
+        } else {
+          call.respond(HttpStatusCode.OK, "")
+        }
       }
 
-      /* GET user input from modal BY ->
-      "view": {
-          "state": {
-              "values": {}
-           }
-       }
-      */
-
       // TODO: add fields validation for presentation.modals
-//      call.respondText(ErrorBuilder.assembleNewOrderError(), ContentType.Application.Json, HttpStatusCode.OK)
     }
   }
 }
